69d52d77432a8349f28eda2c5f15a93bfee5fbe0
Laura Elena Mesa <mesafernandezlauraelena@gmail.com>
2024-07-12 14:02:01 -0500

    Setting global colors and font for the application

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,13 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors: {
+      'black': '#151515',
+      'green': '#4EE1A0',
+      'dark-grey': '#242424',
+      'grey': '#d9d9d9',
+      'white': '#FFFF'
+    }
   },
   plugins: [],
 };
