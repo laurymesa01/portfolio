56ea7c1ae637dc520127658397754e68676b90c2
Laura Elena Mesa <mesafernandezlauraelena@gmail.com>
2024-08-06 12:55:24 -0500

    Fixing some styles

--- src/app/components/Info.tsx
+++ src/app/components/Info.tsx
@@ -5,25 +5,25 @@ import profilePicDesktop from "../../../public/assets/images/image-profile-deskt
 
 export default function Info() {
     return(
-        <section className="relative px-4 md:px-12 lg:px-32 xl:px-40 mt-8  mx-auto p-4 grid grid-rows-2 md:grid-cols-2 md:grid-rows-1">
+        <section className="h-auto relative px-4 md:px-12 lg:px-32 xl:px-40 mt-8  mx-auto p-4 grid grid-rows-2  md:grid-cols-2 md:grid-rows-1">
             <svg className="absolute top-32 right-[50%] md:top-4 md:right-[70%] lg:right-[80%]" xmlns="http://www.w3.org/2000/svg" width="530" height="129"><g fill="none" fillRule="evenodd" stroke="#FFF" opacity=".25"><ellipse cx="265" cy="40" rx="264.5" ry="39.5"/><ellipse cx="265" cy="52" rx="264.5" ry="39.5"/><ellipse cx="265" cy="65" rx="264.5" ry="39.5"/><ellipse cx="265" cy="77" rx="264.5" ry="39.5"/><ellipse cx="265" cy="89" rx="264.5" ry="39.5"/></g>
             </svg>
             <div className='relative md:col-start-2 flex justify-center'>
                 <Image  src={profilePicMobile}
                         alt="Picture of the author"
-                        className='md:hidden '
+                        className='w-[55%] md:hidden '
                 />
                 <Image  src={profilePicTablet}
                         alt="Picture of the author"
-                        className='hidden md:inline-flex lg:hidden'
+                        className='hidden absolute h-full md:inline-flex lg:hidden'
                 />
                 <Image  src={profilePicDesktop}
                         alt="Picture of the author"
-                        className='hidden lg:inline-flex '
+                        className='hidden lg:inline-flex w-[55%]'
                 />
             </div>
             
-            <div className="md:col-start-1 md:row-start-1 flex flex-col items-center md:items-start mt-6 md:mt-0 ">
+            <div className="h-auto md:col-start-1 md:row-start-1  flex flex-col items-center md:items-start mt-6 md:mt-0 ">
                 <h1 className="text-center font-bold text-[40px] md:text-[72px] lg:text-[88px] text-white leading-[40px] md:leading-[72px] lg:leading-[88px] tracking-[-1.14px] md:tracking-[-2.05px] lg:tracking-[-2.5px] md:text-start">Nice to meet you!</h1>
                 <h1 className="text-center font-bold text-[40px] md:text-[72px] lg:text-[88px] text-white leading-[40px] md:leading-[72px] lg:leading-[88px] tracking-[-1.14px] md:tracking-[-2.05px] lg:tracking-[-2.5px] mt-2 md:text-start">
                     <span>I'm </span>
--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -42,7 +42,7 @@ export default function Skills() {
         },
     ]
     return(
-        <section className="mt-24 px-4 md:px-12 lg:px-32 xl:px-40 relative">
+        <section className="px-4 md:px-12 lg:px-32 xl:px-40 relative">
             <svg className="absolute -bottom-16 left-[50%] md:left-[70%] lg:left-[80%] lg:-bottom-10" xmlns="http://www.w3.org/2000/svg" width="530" height="129"><g fill="none" fillRule="evenodd" stroke="#FFF" opacity=".25"><ellipse cx="265" cy="40" rx="264.5" ry="39.5"/><ellipse cx="265" cy="52" rx="264.5" ry="39.5"/><ellipse cx="265" cy="65" rx="264.5" ry="39.5"/><ellipse cx="265" cy="77" rx="264.5" ry="39.5"/><ellipse cx="265" cy="89" rx="264.5" ry="39.5"/></g>
             </svg>
             <div className="border-t border-t-grey border-b border-b-grey md:border-b-black md:grid md:grid-cols-2 lg:grid-cols-3 ">
